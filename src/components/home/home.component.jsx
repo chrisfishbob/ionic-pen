8febe45ac7e1b52927ec4b329a8ad46adbf9c24d
Chris Hsu <chrisfishbob@gmail.com>
2022-10-30 16:04:21 -0700

    Added initial grey bubble implementation

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -2,12 +2,14 @@ import NavScrollExample from "../navbar/navbar.component";
 import RecentlyUpdatedSection from "../recently_updated_section/recently_updated_section.component";
 import PopularSection from "../popular_section/popular_section.component";
 import Footer from "../footer/footer.component";
+import GreyBubble from "../grey_bubble/grey.bubble.component";
 
 
 function Home() {
     return (
         <div>
             <NavScrollExample/>
+            <GreyBubble/>
             <RecentlyUpdatedSection/>
             <PopularSection/>
             <Footer/>
