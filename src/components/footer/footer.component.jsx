5e5947f7476178098e69e8093622b6cb859afab0
Chris Hsu <chrisfishbob@gmail.com>
2022-10-30 19:19:23 -0700

    Added footer easter egg

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,9 @@ import "./footer.styles.css";
 function Footer() {
   return (
     <div className="footer-container">
-      <p> Made By Ionic Pen. © 2022</p>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style={{color: "inherit", textDecoration: "none"}}>
+        <p> Made By Ionic Pen. © 2022</p>
+      </a>
     </div>
   );
 }
