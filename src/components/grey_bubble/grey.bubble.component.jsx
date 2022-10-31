ff1a87a066d1cb8e0ccb5eb42d9b2e55053f4a5a
Chris Hsu <chrisfishbob@gmail.com>
2022-10-30 18:18:44 -0700

    Some slight css fixes when scaling

--- src/components/grey_bubble/grey.bubble.component.jsx
+++ src/components/grey_bubble/grey.bubble.component.jsx
@@ -1,29 +1,34 @@
 import "./grey.bubble.styles.css";
 function GreyBubble() {
-    let one = 1;
+  let one = 1;
   return (
     <div>
       <h4 className="welcome-text"> Welcome PrincessOfStory</h4>
       <div className="grey-bubble-container">
-        <div className="div1">
+        <div className="book-image-container">
           <img
             className="book-image"
             src="https://ionic-pen-public-assets.s3.amazonaws.com/book_cover_8.png"
           />
         </div>
-        <div className="div2">
+        <div className="book-title-container">
           <h4 className="book-title">The Suitcase Switch</h4>
         </div>
-        <div className="div3">
-          <h5 className="info-content">New Readers: {one}</h5>
-          <h5 className="info-content">New Reviews: 2</h5>
-          <h5 className="info-content">New Likes: 3</h5>
+        <div className="new-info-container">
+          {/* We use a inner container here so that it's easy to space the items */}
+          <div className="new-info-inner-container">
+            <h5 className="info-content">New Readers: {one}</h5>
+            <h5 className="info-content">New Reviews: 2</h5>
+            <h5 className="info-content">New Likes: 3</h5>
+          </div>
         </div>
 
-        <div className="div4">
-          <h5 className="info-content">Total Readers: 1</h5>
-          <h5 className="info-content">Total Reviews: 2</h5>
-          <h5 className="info-content">Total Likes: 3</h5>
+        <div className="total-info-container">
+          <div className="total-info-inner-container">
+            <h5 className="info-content">Total Readers: 1</h5>
+            <h5 className="info-content">Total Reviews: 2</h5>
+            <h5 className="info-content">Total Likes: 3</h5>
+          </div>
         </div>
       </div>
     </div>
