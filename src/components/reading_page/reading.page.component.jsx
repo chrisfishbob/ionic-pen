3462c27e72dfd7985cb75899445152e2cf437030
Chris Hsu <chrisfishbob@gmail.com>
2022-11-07 11:23:09 -0800

    Reading page compoenent file added to commit

---
+++ src/components/reading_page/reading.page.component.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../navbar/navbar.component";
+
+function ReadingPage() {
+  return (
+    <div>
+      <NavBar />
+      <h1> AHHHHHHHHHHHHHHH</h1>
+    </div>
+  );
+}
+
+export default ReadingPage;
