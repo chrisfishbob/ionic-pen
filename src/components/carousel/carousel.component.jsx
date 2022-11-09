4cf2f9a8dc498e8ce284cf4a0512def6c699d599
Samay Gupta <samay13g@icoud.com>
2022-11-09 10:57:09 -0800

    Updated Carousel

--- src/components/carousel/carousel.component.jsx
+++ src/components/carousel/carousel.component.jsx
@@ -5,21 +5,33 @@ import { Link } from "react-router-dom";
 
 function ControlledCarousel(props) {
   const [index, setIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(5);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
-
-  let booksByPage = [[]];
+  let booksByPage = [];
+  let booksOnPage = [];
+  for (let i=0; i<pageSize; i++) {
+    booksOnPage.push({isDummy: true});
+  }
+  booksByPage.push(booksOnPage);
   if (props.books && props.books.length > 0) {
     booksByPage = [];
-    let n = 0;
-    for (let i=0; i<props.books.length; i+=5) {
-      booksByPage[n] = [];
-      for (let j=i; j<Math.min(i+5, props.books.length); j++) {
-        booksByPage[n].push(props.books[j]);
+    let booksCount = pageSize;
+    let booksOnPage = []
+    for (let i=0; i<props.books.length; i+=pageSize) {
+      booksOnPage = []
+      booksCount = Math.min(i+pageSize, props.books.length);
+      for (let j=i; j<booksCount; j++) {
+        props.books[j]['isDummy'] = false;
+        booksOnPage.push(props.books[j]);
       }
-      n += 1
+      for (let j=booksCount; j<i+pageSize; j++) {
+        booksOnPage.push({isDummy: true});
+      }
+      booksByPage.push(booksOnPage);
+      console.log(booksOnPage);
     }
   }
  
@@ -36,16 +48,22 @@ function ControlledCarousel(props) {
           <Carousel.Item>
             <div className="carousel-images-container">
               {booksPerPage.map((book) => {
-                return (
-                  <Link to={`/book/${book.book_id}`} key={book.book_id}>
-                    <img
+                if (book['isDummy'])
+                  return <img
                       className="carousel-cover-image"
-                      src={book.cover_image}
                       width="100"
                       height="180"
+                      src = "https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png"
+                      style={{color: 'rgba(0, 0, 0, 0)', border: 0}}
                     />
-                  </Link>
-                );
+                return <Link to={`/book/${book.book_id}`} key={book.book_id}>
+                  <img
+                    className="carousel-cover-image"
+                    src={book.cover_image}
+                    width="100"
+                    height="180"
+                  />
+                </Link>
               })}
             </div>
           </Carousel.Item>
