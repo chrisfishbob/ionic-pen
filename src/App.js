c83eb872f7b3e319bc88c1bd11b469411aae6cda
Amani1031 <aarora09@calpoly.edu>
2022-11-09 19:22:07 -0800

    profile added to app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BookDetail from "./pages/book_detail/book_detail.component";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Session from "./Session";
 import Catalog from "./pages/catalog/catalog.component";
+import Profile from"./pages/user_profile/profile.component";
 
 function Logout() {
   Session.logoutUser();
@@ -20,6 +21,7 @@ function App() {
       <Route path="/catalog" element={<Catalog />} />
       <Route path="/book-details" element={<BookDetail />} />
       <Route path="/book/:book_id" element={<BookDetail />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
