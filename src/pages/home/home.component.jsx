5e5c60faafa699a13f75b1368580b8dbb4c24a3b
Samay Gupta <samay13g@icoud.com>
2022-11-11 11:53:36 -0800

    Search and Carousel updates

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,7 +2,7 @@ import NavBar from "../../components/navbar/navbar.component";
 import Footer from "../../components/footer/footer.component";
 import GreyBubble from "../../components/grey_bubble/grey.bubble.component";
 import IonicPenAPI from "../../IonicPenAPI";
-import ControlledCarousel from "../../components/carousel/carousel.component";
+import BooksCarousel from "../../components/books_carousel/books_carousel.component";
 import { useState, useEffect } from "react";
 import Session from "../../Session";
 import { Navigate } from "react-router-dom";
@@ -39,9 +39,9 @@ function Home() {
       <h4 className="welcome-text"> Welcome {profile.username} </h4>
       <GreyBubble />
       <h1 className="carousel-text">Library</h1>
-      <ControlledCarousel books={library} />
+      <BooksCarousel books={library} />
       <h1 className="carousel-text">All Books</h1>
-      <ControlledCarousel books={books} />
+      <BooksCarousel books={books} />
       <Footer />
     </div>
   );
--- src/pages/search/search.component.jsx
+++ src/pages/search/search.component.jsx
@@ -5,8 +5,24 @@ import IonicPenAPI from "../../IonicPenAPI";
 
 import { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
-import ControlledCarousel from "../../components/carousel/carousel.component";
+import { Card } from "react-bootstrap";
+import PageCarousel from "../../components/page-carousel/page_carousel.component";
+import BooksCarousel from "../../components/books_carousel/books_carousel.component";
 import "./search.styles.css";
+
+
+function generateUsersCarouselObjects(users) {
+    return users.map((user) => {
+        return {
+            isDummy: false,
+            html:  <Card>
+                <Card.Img variant="top" src="https://ionic-pen-public-assets.s3.amazonaws.com/profile.jpeg" />
+                    <Card.Body>{user.username}
+                </Card.Body>
+            </Card>
+        };
+    });
+}
 
 function SearchPage() {
     const [searchResponse, setSearchResponse] = useState({
@@ -19,26 +35,26 @@ function SearchPage() {
     const searchQuery = urlParams.get('q');
     useEffect(() => {
         IonicPenAPI.search(searchQuery).then((response) => {
-            setSearchResponse(response);
+            setSearchResponse({
+                users: generateUsersCarouselObjects(response.users),
+                books: response.books
+            });
         });
-    });
+    }, []);
     return (
         <div>
           <NavBar />
             { searchResponse.users.length > 0 && 
                 <div className="search-container">
                     <h1 className="carousel-text"> Users </h1>
-                    <h3 className="carousel-text"> 
-                        {searchResponse.users.map((user) => {
-                            return user.username
-                        })}
-                    </h3>
+                    <PageCarousel data={searchResponse.users} 
+                        filler={<div className="fixed-box"></div>} />
                 </div>
             }
             { searchResponse.books.length > 0 &&
                 <div className="search-container">
                     <h1 className="carousel-text"> Books </h1>
-                    <ControlledCarousel books={searchResponse.books} />
+                    <BooksCarousel books={searchResponse.books} />
                 </div>
             }
           <Footer />
