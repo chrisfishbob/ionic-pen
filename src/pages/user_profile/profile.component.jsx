087de1b78f43c8bb4d8d6ba790ad4427d0dd61a5
Amani1031 <aarora09@calpoly.edu>
2022-11-14 10:06:44 -0800

    user profile work

--- src/pages/user_profile/profile.component.jsx
+++ src/pages/user_profile/profile.component.jsx
@@ -1,13 +1,29 @@
 import NavBar from "../../components/navbar/navbar.component";
 import Footer from "../../components/footer/footer.component";
 import GreyBubble from "../../components/grey_bubble/grey.bubble.component";
+import DropDown from "../../components/drop_down/dropdown.component";
+import MenuItem from "../../components/menu_item/menu_item.component";
 import IonicPenAPI from "../../IonicPenAPI";
 
 import { useState, useEffect } from "react";
 import Session from "../../Session";
 import { Navigate } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
 
 function Profile() {
+    return (
+        <div>
+          <NavBar />
+          <h4 className="welcome-text"> Welcome {profile.username} </h4>
+          <DropdownMenu option="Profile">
+            <MenuItem text="Edit Profile" location="" />
+            <MenuItem text="Change Password" location="" />
+            <MenuItem text="Privacy Settings" location="" />
+            <MenuItem text="Delete Account" onClick={} />
+            </DropdownMenu>
+          <Footer />
+        </div>
+      );
 
 }
 
